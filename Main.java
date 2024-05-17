83c6f0b01fe418ac75b284aa6a554e19185c919f
Dziaba <wyrwisz.hubert@gmail.com>
2024-05-17 17:51:33 +0000

    zad3

--- Main.java
+++ Main.java
@@ -1,35 +1,19 @@
-import company.models.Manager;
 import company.models.Worker;
-import company.abstracts.Employee;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main{
     public static void main(String[] args){
         Worker work1 = new Worker("Daniel ", 1234,"04-09-2022", "Baker", 1);
-        Worker work2 = new Worker("Bob ", 23456, "05-09-2022", "MAin Chef", 2);
+        Worker work2 = new Worker("Bob ", 23456, "05-09-2022", "MAin Chef", 1);
         Worker work3 = new Worker("MAt ", 12134,"06-09-2022", "Kitchen Help", 3);
-        Worker work4 = new Worker("Tomasz ", 7897, "07-09-2022", "Waiter", 4);
 
-        Manager manager = new Manager("Hubert ", 99999, "03-09-2022", "Boss of the bosses", 5);
+        System.out.println(work1.getName() + " has code: " + work1.hashCode());
+        System.out.println(work2.getName() + " has code: " + work2.hashCode());
+        System.out.println(work3.getName() + " has code: " + work3.hashCode());
 
-        List<Employee> employees = new ArrayList<>();
-        employees.add(work1);
-        employees.add(work2);
-        employees.add(work3);
-        employees.add(work4);
-        employees.add(manager);
-    
-        for (Employee employee: employees){
-            employee.work();
-            System.out.println(
-                employee.getName() + " ID: " 
-                + employee.hashCode()+ 
-                " Position: " + employee.getPosition() + 
-                " ,Hire date: " + employee.getHireDate() + 
-                " Salary: " + employee.getSalary() );
-        }
+        System.out.println(" work 1 comp work2 " + work1.equals(work2));
+        System.out.println(" work 1 comp work3 " + work1.equals(work3));
+        System.out.println(" work 2 comp work3 " + work2.equals(work3));
 }
 
 
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -31,5 +31,15 @@ public abstract class Employee implements Employable {
     public int hashCode(){
         return id;
     }
-    
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) obj;
+            return id == other.id;
+    }
 }
